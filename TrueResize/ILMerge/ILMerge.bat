1307b4ab99d20af4097d46e5b4a26da13ae97889
Tal Aloni <tal.aloni.il@gmail.com>
2016-12-27 11:17:42 +0200

    Moved ILMerge.bat

--- TrueResize/ILMerge/ILMerge.bat
+++ TrueResize/ILMerge/ILMerge.bat
@@ -1,5 +1,5 @@
-set binaryPath=%CD%\..\TrueResize\bin\Release
-set outputPath=%CD%\..\TrueResize\bin\ILMerge
+set binaryPath=%CD%\..\bin\Release
+set outputPath=%CD%\..\bin\ILMerge
 IF NOT EXIST "%outputPath%" MKDIR "%outputPath%"
 IF ["%programfiles(x86)%"]==[""] SET ilmergePath="%programfiles%\Microsoft\ILMerge"
 IF NOT ["%programfiles(x86)%"]==[""] SET ilmergePath="%programfiles(x86)%\Microsoft\ILMerge"
